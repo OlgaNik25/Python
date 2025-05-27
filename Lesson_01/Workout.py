20174b6ca98244a6d453436fec4c7789aedc5036
OlgaNik25 <filimonova.o.n@yandex.ru>
2025-05-27 18:44:08 +0800

    Тренировочные упражнения

---
+++ Lesson_01/Workout.py
@@ -0,0 +1,29 @@
+#1
+my_heigh=155
+print(my_heigh)
+
+#2
+my_name="Ольга"
+my_name="Ольга Филимонова"
+print(my_name)
+
+#3
+pet_name=input("Как зовут вашего питомца?")
+print("Ваш любимчик -", pet_name)
+
+#4
+def print_python():
+    print("Учу Python!")
+
+print_python()
+
+#5
+def print_letter(let):
+    print(let, end="")
+
+print_letter('S')
+print_letter('k')
+print_letter('y')
+print_letter('P')
+print_letter('r')
+print_letter('o')
